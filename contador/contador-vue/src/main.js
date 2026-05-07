3c700e93e43fb4b6710f9d7b454cfd727c490b21
Augusto Lopes <augustolfp@gmail.com>
2026-05-06 23:01:24 -0300

    feat: add base css config

--- contador/contador-vue/src/main.js
+++ contador/contador-vue/src/main.js
@@ -3,6 +3,10 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 
+import "./assets/reset.css";
+import "./assets/base.css";
+import "./assets/utilityClasses.css";
+
 new Vue({
   router,
   render: (h) => h(App),
