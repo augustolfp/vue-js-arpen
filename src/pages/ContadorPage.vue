3da394e9e64cc466ff910f3d3304fccd0e539573
Augusto Lopes <augustolfp@gmail.com>
2026-05-06 21:57:39 -0300

    feat: add estilo ao contador

--- src/pages/ContadorPage.vue
+++ src/pages/ContadorPage.vue
@@ -1,6 +1,65 @@
+<script>
+export default {
+}
+</script>
+
 <template>
-  <div>
-    <h1>Salve quebrada</h1>
-    <h2>Exemplo: Contador</h2>
+  <div class="contador-container">
+    <h1>Exemplo: Contador</h1>
+
+    <div class="contagem">
+      <h2>CONTAGEM ATUAL:</h2>
+      <div> 0 </div>
+    </div>
+
+    <div class="controls">
+      <button> + </button>
+      <button> - </button>
+    </div>
   </div>
 </template>
+
+<style scoped>
+.contador-container>* {
+  margin: auto;
+}
+
+button {
+  background-color: #3498db;
+  color: white;
+  border: none;
+  padding: 12px 24px;
+  border-radius: 8px;
+  font-size: large;
+  font-weight: bold;
+}
+
+button:hover {
+  background-color: #2980b9;
+  cursor: pointer;
+}
+
+.controls {
+  display: flex;
+  justify-content: center;
+  gap: 12px;
+  margin-top: 24px;
+}
+
+.contagem {
+  margin-top: 24px;
+  background-color: grey;
+  padding: 24px;
+  max-width: 300px;
+  border-radius: 24px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.contagem>div {
+  font-weight: bolder;
+  font-size: 48px;
+  color: white;
+}
+</style>
